5d65eb7d0f0f1f2282216af9b5b4f9beddb97431
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-09-27 10:25:10 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  before_action :authorize
+  
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  # def record_not_found(err)
+  #   render json: { error: "#{err.model} not found" }, status: :not_found
+  # end
+
+  def invalid_record(err)
+    render json: { errors: err.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
